f43ee878bfb4922aef25bd94ea733bcf928d19eb
Vitório Corrêa <97925377+VitorioCp@users.noreply.github.com>
2025-07-23 14:55:33 -0300

    feat: enable CORS

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,24 @@
-import express from 'express';
-import authRoutes from './routes/auth.routes';
-import privateRoutes from './routes/auth.routes';
-import { initDb } from './db/database';
-import { config } from './config/env';
-
+import express from "express";
+import authRoutes from "./routes/auth.routes";
+import privateRoutes from "./routes/auth.routes";
+import { initDb } from "./db/database";
+import { config } from "./config/env";
+import cors from "cors";
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
-app.use('/auth', authRoutes);
-app.use('/private', privateRoutes);
+app.use("/auth", authRoutes);
+app.use("/private", privateRoutes);
 
-app.get('/', (req, res) => res.send('API estruturada com REST'));
+app.get("/", (req, res) => res.send("API estruturada com REST"));
 
 initDb().then(() => {
   app.listen(config.port, () => {
