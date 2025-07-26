41ffee8817efb0f72763a3fd185a46fd0d8aa635
Vitório Corrêa <97925377+VitorioCp@users.noreply.github.com>
2025-07-25 22:50:39 -0300

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://course-manager-front-end-main.vercel.app/"],
+    origin: ["http://localhost:3000","https://course-manager-front-end-main.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
